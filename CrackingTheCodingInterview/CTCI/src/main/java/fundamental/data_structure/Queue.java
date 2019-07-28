4f9f92cff584ecc473723fd815a5d16d2121a37b
Mislav Jaksic <jaksicmislav@gmail.com>
2019-07-28 23:37:07 +0200

    Started constructing a Queue

---
+++ CrackingTheCodingInterview/CTCI/src/main/java/fundamental/data_structure/Queue.java
@@ -0,0 +1,19 @@
+package fundamental.data_structure;
+
+public class Queue {
+	public void add(int number) {
+		
+	}
+	
+	public int remove() {
+		
+	}
+	
+	public int peek() {
+		
+	}
+	
+	public boolean isEmpty() {
+		
+	}
+}
