db5c7d8e6c57a48f056427d3c09ab23a35cef848
Mislav Jaksic <jaksicmislav@gmail.com>
2019-08-02 09:55:38 +0200

    Started 3.4

---
+++ CrackingTheCodingInterview/CTCI/src/main/java/stackqueue/QueueViaStacks.java
@@ -0,0 +1,31 @@
+package stackqueue;
+
+import fundamental.data_structure.Stack;
+
+public class QueueViaStacks {
+	private Stack push_stack;
+	private Stack pop_stack;
+	
+	
+	
+	public QueueViaStacks() {
+		this.push_stack = new Stack();
+		this.pop_stack = new Stack();
+	}
+	
+	public void add(int data) {
+		
+	}
+	
+	public int remove() {
+		
+	}
+	
+	public int peek() {
+		
+	}
+	
+	public boolean isEmpty() {
+		
+	}
+}
