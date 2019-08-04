f4c05618dd3d6f7e48539f06db59e9ebf32acc57
Mislav Jaksic <jaksicmislav@gmail.com>
2019-08-04 23:44:27 +0200

    Solved up to 3.4

--- CrackingTheCodingInterview/CTCI/src/main/java/stackqueue/QueueViaStacks.java
+++ CrackingTheCodingInterview/CTCI/src/main/java/stackqueue/QueueViaStacks.java
@@ -3,29 +3,45 @@ package stackqueue;
 import fundamental.data_structure.Stack;
 
 public class QueueViaStacks {
-	private Stack push_stack;
-	private Stack pop_stack;
+	private Stack new_stack;
+	private Stack old_stack;
 	
 	
 	
 	public QueueViaStacks() {
-		this.push_stack = new Stack();
-		this.pop_stack = new Stack();
+		this.new_stack = new Stack();
+		this.old_stack = new Stack();
 	}
 	
 	public void add(int data) {
-		
+		this.new_stack.push(data);
 	}
 	
 	public int remove() {
+		if (this.old_stack.isEmpty()) {
+			this.transfer();
+		}
 		
+		return this.old_stack.pop();
 	}
 	
 	public int peek() {
+		if (this.old_stack.isEmpty()) {
+			this.transfer();
+		}
 		
+		return this.old_stack.peek();
 	}
 	
 	public boolean isEmpty() {
-		
+		return (this.new_stack.isEmpty() && this.old_stack.isEmpty());
+	}
+	
+	
+	
+	private void transfer() {
+		while (!this.new_stack.isEmpty()) {
+			this.old_stack.push(this.new_stack.pop());
+		}
 	}
 }
--- CrackingTheCodingInterview/CTCI/src/test/java/stack/StackProblems.java
+++ CrackingTheCodingInterview/CTCI/src/test/java/stack/StackProblems.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import fundamental.data_structure.Stack;
+import stackqueue.QueueViaStacks;
 import stackqueue.SetOfStacks;
 import stackqueue.StackMin;
 
@@ -153,6 +154,56 @@ public class StackProblems {
 			boolean expected = false;
 			
 			assertEquals(expected, stack.isEmpty());
+		}
+	}
+	
+	@Nested
+	class QueueViaStacksTests {
+		QueueViaStacks queue = new QueueViaStacks();
+		
+		@Test
+		public void manyPopTest() {
+			int data = 1;
+			queue.add(data);
+			data = 2;
+			queue.add(data);
+			
+			int expected = 1;
+			
+			assertEquals(expected, queue.remove());
+		}
+		
+		@Test
+		public void manyPeekTest() {
+			int data = 1;
+			queue.add(data);
+			data = 2;
+			queue.add(data);
+			data = 3;
+			queue.add(data);
+			data = 4;
+			queue.add(data);
+			
+			int expected = 1;
+			
+			assertEquals(expected, queue.peek());
+		}
+		
+		@Test
+		public void emptyTest() {
+			boolean expected = true;
+			
+			assertEquals(expected, queue.isEmpty());
+		}
+		
+		@Test
+		public void notEmptyTest() {
+			int data = 1;
+			queue.add(data);
+			
+			boolean expected = false;
+			
+			assertEquals(expected, queue.isEmpty());
 		}
 	}
 }
